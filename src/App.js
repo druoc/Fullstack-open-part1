da8e825487bce8cfe1a0493c307db8ce47ebd00a
Dave <d.ruocco1987@googlemail.com>
2022-07-28 14:12:41 +0100

    Excercise 1.6, moved Statistics section into its own component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import {useState} from 'react';
 
+const Statistics = (props) => {
+  return (
+    <p>{props.name} = {props.value}</p>
+  );
+};
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
+  const average = (good * 1 + bad * -1) / total;
+  const positive = good * (100/total);
 
   //components
   const Button = (props) => {
@@ -26,17 +35,22 @@ const App = () => {
     setBad(bad + 1);
   };
 
+  
+
   return (
     <div>
       <h1>Give Feedback</h1>
-      <Button buttonName="Good" buttonFunction={goodClick}/>
+      <Button buttonName="Good" buttonFunction={goodClick} />
       <Button buttonName="Neutral" buttonFunction={neutralClick} />
       <Button buttonName="Bad" buttonFunction={badClick} />
 
       <h1>Statistics</h1>
-      <p>Good = {good}</p>
-      <p>Neutral = {neutral}</p>
-      <p>Bad = {bad}</p>
+      <Statistics name="Good" value={good} />
+      <Statistics name="Neutral" value={neutral} />
+      <Statistics name="Bad" value={bad} />
+      <Statistics name="Total" value={total} />
+      <Statistics name="Average" value={average} />
+      <Statistics name="Positive %" value={positive} />
 
 
     </div>
