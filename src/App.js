3244a7eff200d150f8bc8ed264686892bd213cd7
Dave <d.ruocco1987@googlemail.com>
2022-07-28 13:24:47 +0100

    Excercise 1.6, set up UI and button functionality

--- src/App.js
+++ src/App.js
@@ -1,62 +1,44 @@
 import React from 'react';
+import {useState} from 'react';
 
 const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  };
-
-
-
-  const Header = (props) => {
+  //components
+  const Button = (props) => {
     return (
-      <h1>{props.course.name}</h1>
+      <button onClick={props.buttonFunction}>{props.buttonName}</button>
     );
   };
 
-  const Part = (props) => {
-    return (
-        <p>
-          {props.section} {props.exercises}
-        </p>
-    );
+  //functions
+  const goodClick = () => {
+    setGood(good + 1);
   };
 
-  const Content = () => {
-    return (
-      <>
-        <Part section={course.parts[0].name} exercises={course.parts[0].exercises} />
-        <Part section={course.parts[1].name} exercises={course.parts[1].exercises} />
-        <Part section={course.parts[2].name} exercises={course.parts[2].exercises} />
-      </>
-    );
+  const neutralClick = () => {
+    setNeutral(neutral + 1);
   };
 
-  const Total = () => {
-    return (
-      <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
-    );
+  const badClick = () => {
+    setBad(bad + 1);
   };
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={course.parts}/>
-      <Total parts={course.parts}/>
+      <h1>Give Feedback</h1>
+      <Button buttonName="Good" buttonFunction={goodClick}/>
+      <Button buttonName="Neutral" buttonFunction={neutralClick} />
+      <Button buttonName="Bad" buttonFunction={badClick} />
+
+      <h1>Statistics</h1>
+      <p>Good = {good}</p>
+      <p>Neutral = {neutral}</p>
+      <p>Bad = {bad}</p>
+
+
     </div>
   );
 
