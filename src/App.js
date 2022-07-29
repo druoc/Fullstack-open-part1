2c0aadea125cda57d85bb661f45d6ba93447b168
Dave <d.ruocco1987@googlemail.com>
2022-07-29 12:37:17 +0100

    Added conditional rendering to Statistics component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import {useState} from 'react';
 
-const Statistics = (props) => {
-  return (
-    <p>{props.name} = {props.value}</p>
-  );
-};
-
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -16,6 +10,12 @@ const App = () => {
   const positive = good * (100/total);
 
   //components
+  const Statistics = (props) => {
+    return (
+        <p>{props.name} = {props.value}</p>
+      );
+  };
+
   const Button = (props) => {
     return (
       <button onClick={props.buttonFunction}>{props.buttonName}</button>
@@ -35,6 +35,26 @@ const App = () => {
     setBad(bad + 1);
   };
 
+  const StatsRender = () => {
+    if (total === 0) {
+      return (<p>No feedback given</p>);
+    } else {
+      return (
+        <div className="stats--container">
+        <h1>Statistics</h1>
+          <Statistics name="Good" value={good} />
+          <Statistics name="Neutral" value={neutral} />
+          <Statistics name="Bad" value={bad} />
+          <Statistics name="Total" value={total} />
+          <Statistics name="Average" value={average} />
+          <Statistics name="Positive %" value={positive} />
+        </div>
+      );
+      
+    }
+    
+  }
+
   
 
   return (
@@ -44,15 +64,7 @@ const App = () => {
       <Button buttonName="Neutral" buttonFunction={neutralClick} />
       <Button buttonName="Bad" buttonFunction={badClick} />
 
-      <h1>Statistics</h1>
-      <Statistics name="Good" value={good} />
-      <Statistics name="Neutral" value={neutral} />
-      <Statistics name="Bad" value={bad} />
-      <Statistics name="Total" value={total} />
-      <Statistics name="Average" value={average} />
-      <Statistics name="Positive %" value={positive} />
-
-
+      <StatsRender />
     </div>
   );
 
